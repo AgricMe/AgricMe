0c8ca90aff69613cbdeb5a235c140767f35d0d51
Ibrahim Oluwadurotimi <Ibrahimdurotimi1@gmail.com>
2024-02-12 12:05:53 +0100

    section3

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from '@/components/home/Nav'
 import Hero from '@/components/home/Hero'
+import ThirdSection from '@/components/home/thirdSection'
 
 function Home() {
   return (
     <div>
       <Nav />
       <Hero />
+      <ThirdSection />
     </div>
   )
 }
---
+++ components/home/thirdSection.tsx
@@ -0,0 +1,27 @@
+import "../css/thirdSection.css"
+
+const ThirdSection = () => {
+  return (
+    <section className="sec">
+        <article className="art">
+            <div className="div">
+                <section className="sect">
+                    <span>
+                        <div></div>
+                    </span>
+                    <span>
+                        <div></div>
+                    </span>
+                    <span>
+                        <div></div>
+                    </span>
+                </section>
+            </div>
+            <div className="div"></div>
+            <div className="div"></div>
+        </article>
+    </section>
+  )
+}
+
+export default ThirdSection
