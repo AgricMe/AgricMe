68443a94b2a39b38f0ae76801f41c588d4b2897b
Dev-Natha <maranathaayilara@gmail.com>
2024-02-14 14:49:03 -0800

    nath is a foooooool

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,19 @@ import Nav from '@/components/home/Nav'
 import Hero from '@/components/home/Hero'
 import FirstSection from '@/components/home/FirstSection'
 import AnimationWrapper from './AnimationWrapper'
+import ThirdSection from '@/components/home/thirdSection'
+import SecondSection from '@/components/home/secondSection'
+
 function Home() {
   return (
     <div>
       <Nav />
       <AnimationWrapper>
         <Hero />
+        <FirstSection />
+        <SecondSection />
+        <ThirdSection />
       </AnimationWrapper>
-      <FirstSection />
     </div>
   )
 }
---
+++ components/home/secondSection.tsx
@@ -0,0 +1,25 @@
+
+const SecondSection = () => {
+  return (
+    <section className="blog">
+        <section>
+            <div className="blogTop">
+                <span>News</span>
+                <div>
+                    <span>Discover weekly content about organic food, & more</span>
+                    <button className="bottom">
+                        <span>More news</span>
+                        <div><svg width="9" height="8" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.4769 1.12891L7.87144 4.19342L4.4769 7.25794M7.39998 4.19342H0.516602" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        </div>
+                    </button>
+                </div>
+            </div>
+            <div className="blogMid"></div>
+        </section>
+    </section>
+  )
+}
+
+export default SecondSection
