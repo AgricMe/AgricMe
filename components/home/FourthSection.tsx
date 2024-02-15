98e76e088933a9f9801d921e84778a8ee215d51b
Dev-Natha <maranathaayilara@gmail.com>
2024-02-15 10:43:18 -0800

    Testing

--- components/home/FourthSection.tsx
+++ components/home/FourthSection.tsx
@@ -2,18 +2,27 @@ import Image from "next/image"
 const FourthSection = () => {
   return (
     <div className="fourth-sec">
-        <Image
-        src={"/component-images/fourth-sec.png"}
-        alt="fourth-sec"
-        className="abs-img-left"
-        fill 
-        />
-        <Image
-        src={"/component-images/fourth-sec2.png"}
-        alt="fourth-sec2"
-        className="abs-img-right"
-        fill 
-        />
+        <div>
+            <Image
+            src={"/component-images/fourth-sec.png"}
+            alt="fourth-sec"
+            className="abs-img-left"
+            fill 
+            />
+            <Image
+            src={"/component-images/fourth-sec2.png"}
+            alt="fourth-sec2"
+            className="abs-img-right"
+            fill 
+            />
+        </div>
+        <div className="fourth-sec-items">
+            <p className="custom-txt"></p>
+            <div className="top">
+                <span>What Our Customer Saying</span>
+                <span>What Our Customer Saying</span>
+            </div>
+        </div>
     </div>
   )
 }
