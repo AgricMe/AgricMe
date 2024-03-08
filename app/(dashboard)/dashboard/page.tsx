033d5c5632dbe7c0f5944ad3ed2696a06cb3cf11
Dev-Natha <maranathaayilara@gmail.com>
2024-03-08 12:38:08 -0800

    Err.. idk

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -10,10 +10,15 @@ const page = () => {
             </div>
             {/* Posts Section */}
             <div className="flex justify-between mt-[40px]">
-                <div className="w-[73%] h-[700px] bg-slate-900"></div>
+                <div className="w-[73%] h-[700px]">
+                    <div className="w-full h-[100px] bg-[#F9F8F8]" style={{height:"70px", backgroundColor:"#F9F8F8", boxShadow:"2px 2px 2px rgba(0, 0, 0, 0.25)", borderRadius:"5px"}}>
+                       
+                    </div>
+                </div>
                 <div className="w-[18%] h-32 bg-emerald-900 fixed right-6"></div>
             </div>
         </div>
+        
     )
 }
 
