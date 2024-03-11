878656f648b9b2cf8d525044841c8bc46de7ff36
Dev-Natha <maranathaayilara@gmail.com>
2024-03-10 19:57:23 -0700

    right side bar added

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -12,13 +12,36 @@ const page = () => {
             </div>
             {/* Posts Section */}
             <div className="flex justify-between mt-[40px]">
-                <div className="w-[73%]">
+                <div className="w-[72%]">
                     <UploadPost />
                     <PostComponent />
                     <PostComponent />
                     <PostComponent />
                 </div>
-                <div className="w-[18%] h-32 bg-emerald-900 fixed right-6"></div>
+                <div className="w-[19%] fixed right-6">
+                    <div className="bg-white rounded-lg shadow-md w-full">
+                        <div className="p-3 text-black">
+                            <h2 className=" font-semibold">Trends For You</h2>
+                        </div>
+                        <div>
+                            <div className="px-3 pb-2 hover:bg-slate-200 text-slate-800">
+                                <p>Only On Agric me</p>
+                                <h2 className="font-semibold text-black">#IncreaseInSales</h2>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus, fuga.</p>
+                            </div>
+                            <div className="px-3 pb-2 hover:bg-slate-200 text-slate-800">
+                                <p>Only On Agric me</p>
+                                <h2 className="font-semibold text-black">#IncreaseInSales</h2>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus, fuga.</p>
+                            </div>
+                            <div className="px-3 pb-2 hover:bg-slate-200 text-slate-800">
+                                <p>Only On Agric me</p>
+                                <h2 className="font-semibold text-black">#IncreaseInSales</h2>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus, fuga.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         
