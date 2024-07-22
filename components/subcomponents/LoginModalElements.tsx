d2ba2bcf02a566babd84f596c3f8df3f7fff3654
Dev-Natha <maranathaayilara@gmail.com>
2024-07-22 01:02:07 -0700

    Deployment issues fixed

--- components/subcomponents/LoginModalElements.tsx
+++ components/subcomponents/LoginModalElements.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Modal, ModalBody, ModalContent, ModalFooter, ModalTrigger } from "../subcomponents/LoginModal";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { useRouter } from 'next/navigation'
 
 export default function LoginModalElements() {
   const images = [
@@ -12,6 +13,7 @@ export default function LoginModalElements() {
     "",
     "",
   ];
+  const router = useRouter()
   return (
     <div className="">
       <Modal>
@@ -101,8 +103,8 @@ export default function LoginModalElements() {
             </div>
           </ModalContent>
           <ModalFooter className="gap-4">
-            <button className="px-2 py-1 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300 rounded-md text-sm w-28">
-              Sign
+            <button className="px-2 py-1 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300 rounded-md text-sm w-28" onClick={() => router.push("/agrions")}>
+              Dashboard
             </button>
             <button className="bg-black text-white dark:bg-white dark:text-black text-sm px-2 py-1 rounded-md border border-black w-28">
               Register
