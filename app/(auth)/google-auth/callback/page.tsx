9f51b94725f0d97c15a5fa1a2505864f08414ab5
Adesugba Samuel <adeolasam20@gmail.com>
2024-12-22 19:55:45 +0100

    fix: build error

--- app/(auth)/google-auth/callback/page.tsx
+++ app/(auth)/google-auth/callback/page.tsx
@@ -5,20 +5,20 @@ import { TokenStorage } from '@/lib/utils/localStorage';
 import { useParams, useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from "react";
 
-export default function Page() {
+const Page = () => {
   const { push } = useRouter();
-  const access_token = useSearchParams().get('access_token');
-  console.log(access_token);
-  
+  const access_token = useSearchParams().get('access_token');  
 
   useEffect(() => {
     if (access_token) {
       TokenStorage.store(access_token);
       push("/dashboard"); 
     }
-  }, [push, access_token]);
+  },[]);
 
   return <div className='flex justify-center items-center h-screen'>
     <DotLoader  />    
   </div>
 };
+
+export default Page;
