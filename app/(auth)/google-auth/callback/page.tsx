ce42c11b14db853f5c47df2bd2dd688efed231fc
samscript18 <adeolasam20@gmail.com>
2025-01-02 23:27:33 +0100

    feat: google oauth callback

--- app/(auth)/google-auth/callback/page.tsx
+++ app/(auth)/google-auth/callback/page.tsx
@@ -3,22 +3,27 @@
 import DotLoader from '@/components/shared/dot-loader';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 export default function Page() {
 	const { push } = useRouter();
-  const isAuthenticated = useSearchParams().get('isAuthenticated');
-  console.log(isAuthenticated);
+	const access_token = useSearchParams().get('access_token');
 
 	useEffect(() => {
 		const storeToken = async () => {
-			if (isAuthenticated) {
+			if (access_token) {
+				Cookies.set('access_token', access_token, {
+					expires: 1,
+					secure: true,
+					sameSite: 'none',
+				});
 				push('/dashboard');
 			} else {
 				push('/login');
 			}
 		};
 		storeToken();
-	}, [push, isAuthenticated]);
+	}, [push, access_token]);
 
 	return (
 		<div className="flex justify-center items-center h-screen">
