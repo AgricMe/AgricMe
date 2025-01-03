1a8f615c51edec4ab7107d5dc4206aec4ba5f8f2
samscript18 <adeolasam20@gmail.com>
2025-01-03 18:14:28 +0100

    feat: not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,36 @@
+import Footer from '@/components/home/NotFoundFooter';
+import Nav from '@/components/home/NotFoundNav';
+import { ButtonContained } from '@/components/shared/Button';
+import Link from 'next/link';
+import { FaCircleArrowRight } from 'react-icons/fa6';
+
+const NotFoundPage = () => {
+	return (
+		<section>
+			<Nav />
+			<div
+				className="relative w-full h-[90vh] flex justify-end items-start pt-[3rem]"
+				style={{
+					backgroundImage: `url('/component-images/not-found-img.png')`,
+					backgroundPosition: 'center',
+					backgroundSize: 'cover',
+				}}>
+				<div className="flex flex-col pr-[4.5rem]">
+					<h1 className="text-[12rem] text-[#8FA8A8] font-extrabold">404</h1>
+					<h2 className="text-[#274C5B] text-[2.8rem] font-semibold mt-[-2rem]">Page Not Found</h2>
+					<p className="text-[#525C60] text-[14px] mb-4">
+						The page you are looking for doesn't exist or has been moved
+					</p>
+					<Link href={'/'} className="w-[40%]">
+						<ButtonContained>
+							Go to Homepage
+							<FaCircleArrowRight size={18} className="text-[#335B6B]" />
+						</ButtonContained>
+					</Link>
+				</div>
+			</div>
+			<Footer />
+		</section>
+	);
+};
+export default NotFoundPage;
---
+++ components/home/NotFoundFooter.tsx
@@ -0,0 +1,113 @@
+import Image from 'next/image';
+import { CgCopyright } from 'react-icons/cg';
+import { FaFacebook, FaInstagramSquare, FaTwitter } from 'react-icons/fa';
+import { FaPinterest } from 'react-icons/fa';
+
+const Footer = () => {
+	return (
+		<section
+			id="contact"
+			className="footer bg-[#fff] w-full flex flex-col px-[1.5rem] md:px-[3rem] lg:px-[4rem]">
+			<section className="w-full grid grid-cols-1 lg:grid-cols-[auto,700px,auto]">
+				<article className="w-full relative lg:p-[30px] col-span-1">
+					<h1>Contact Us</h1>
+					<div>
+						<p>Email</p>
+						<p>needhelp@agricme.com</p>
+					</div>
+					<div>
+						<p>Phone</p>
+						<p>+234 123 456 789</p>
+					</div>
+					<div>
+						<p>Address</p>
+						<p>NCFF House, Imo State.</p>
+					</div>
+				</article>
+				<div className="hidden lg:flex relative w-full h-full">
+					<div className="line2 hidden lg:flex"></div>
+					<article className="p-[30px] col-span-1">
+						<div style={{ alignItems: 'center' }}>
+							<Image src="/logo/agricme-logo.png" alt="logo" width={160} height={36.91} />
+						</div>
+						<div
+							style={{
+								textAlign: 'center',
+								color: '#525C60',
+								fontSize: '13px',
+								fontWeight: '400',
+							}}>
+							AgricMe stands as an innovative and purpose-driven agricultural social platform, meticulously
+							designed to revolutionize farmer interactions and trade within the agricultural sector.
+						</div>
+						<h3 className="agri">
+							<span className="agri1">
+								<FaInstagramSquare className="iconfoot" />
+							</span>
+							<span className="agri1">
+								<FaFacebook className="iconfoot" />
+							</span>
+							<span className="agri1">
+								<FaTwitter className="iconfoot" />
+							</span>
+							<span className="agri1">
+								<FaPinterest className="iconfoot" />
+							</span>
+						</h3>
+					</article>
+				</div>
+				<div className="w-full relative">
+					<div className="line2 hidden lg:flex"></div>
+					<article className="py-[30px] col-span-1 lg:ml-[6rem]">
+						<h1>Utility Pages</h1>
+						<span>Style Guides</span>
+						<span>404 Not Found</span>
+						<span>Password Protected</span>
+						<span>Licenses</span>
+						<span>Changelog</span>
+					</article>
+				</div>
+				<div className="lg:hidden relative w-full h-full">
+					<div className="line2 hidden lg:flex"></div>
+					<article className="lg:p-[30px] col-span-1">
+						<div style={{ alignItems: 'center' }}>
+							<Image src="/logo/agricme-logo.png" alt="logo" width={160} height={36.91} />
+						</div>
+						<div
+							style={{
+								textAlign: 'center',
+								color: '#525C60',
+								fontSize: '13px',
+								fontWeight: '400',
+							}}>
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga dolorem rem voluptatum magnam
+							maiores, earum, magni culpa quas ad.
+						</div>
+						<h3 className="agri">
+							<span className="agri1">
+								<FaInstagramSquare className="iconfoot" />
+							</span>
+							<span className="agri1">
+								<FaFacebook className="iconfoot" />
+							</span>
+							<span className="agri1">
+								<FaTwitter className="iconfoot" />
+							</span>
+							<span className="agri1">
+								<FaPinterest className="iconfoot" />
+							</span>
+						</h3>
+					</article>
+				</div>
+			</section>
+			<div className="mt-0 lg:mt-[50px] w-full h-[60px] flex justify-center items-center border-t border-[#525c60]">
+				<span className="text-center text-[#274c5b] flex items-center text-[15px] font-normal">
+					Copyright <CgCopyright />
+					<span className="text-[18px] font-bold">AgricMe</span>
+				</span>
+			</div>
+		</section>
+	);
+};
+
+export default Footer;
